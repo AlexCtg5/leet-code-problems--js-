4dfd9e4cea541ec902a5e1f84d1dceef85f2b180
Catangiu Alex <catangiu.alex@yahoo.com>
2024-05-23 13:20:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const nums = [1, 7, 3, 6, 5, 6];
+
+var pivotIndex = function (nums) {
+  let totalSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    totalSum += nums[i];
+  }
+  let leftSum = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (leftSum * 2 == totalSum - nums[i]) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+};
+
+console.log(pivotIndex(nums));
